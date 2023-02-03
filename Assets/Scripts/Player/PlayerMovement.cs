24723f591887c8aed5f29eccd4d184fcc12f5a95
Dante Cash <32469207+DanteCash98@users.noreply.github.com>
2023-02-02 20:19:47 -0600

    Added new Input to Player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,42 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-
+    public InputActionAsset inputActionMap;
     private float horizontalMove = 0.0f;
     public Animator Animator;
 
-    public float runSpeed = 10f;
+    [SerializeField] private float runSpeed = 10f;
     private bool jump;
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
+        inputActionMap["Player/Jump"].performed += OnJump;
         
+        inputActionMap.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed * Time.fixedDeltaTime;
-        jump = Input.GetButton("Jump");
-        Animator.SetFloat("Speed", Math.Abs(horizontalMove));
+        inputActionMap["Player/Jump"].performed -= OnJump;
         
-        if (jump)
-        {
-            Animator.SetBool("Jump", true);
-        }
+        inputActionMap.Disable();
     }
-
+    
     private void FixedUpdate()
     {
         controller.Move(horizontalMove , jump);
+        Animator.SetFloat("Speed", Math.Abs(horizontalMove));
+        horizontalMove = inputActionMap["Player/Move"].ReadValue<Vector2>().x * runSpeed * Time.fixedDeltaTime;
+        
+        
     }
 
     public void Landed()
     {
         Animator.SetBool("Jump", false);
+        jump = false;
+    }
+
+    private void OnJump(InputAction.CallbackContext ctx)
+    {
+        Animator.SetBool("Jump", true);
+        jump = true;
     }
 }
