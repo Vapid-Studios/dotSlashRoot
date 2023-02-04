e602bfea58bcfa8f9d726b549be7d50620b273b2
Elijah Gaytan <elijahgaytan@utexas.edu>
2023-02-04 01:55:49 -0600

    made changes to worm anim to remove root animimation

---
+++ Assets/Scripts/Enemies/Worm.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worm : Enemy
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Move()
+    {
+        
+    }
+
+    public override void Patrol()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
